a7cf6c776f7dc5fef2d77e31a931f9fccd7d1a86
Dhairya Gothi <dhairyag31@gmail.com>
2024-09-15 14:57:40 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react' 
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>This is station saarthi your platform guide
+     </div>
+    </>
+  )
+}
+
+export default App
