418440ef6d4aefddf98a4ef0e68131ce480b477b
Dhairya Gothi <dhairyag31@gmail.com>
2024-09-30 14:58:41 +0530

    hamburger

--- frontend/src/Pages/hamburger.jsx
+++ frontend/src/Pages/hamburger.jsx
@@ -19,6 +19,7 @@ const HamburgerMenu = () => {
                         checked={isOpen}
                         onChange={toggleMenu}
                         id="menuToggle"
+                        className={`absolute ${isOpen ? 'top-0 left-[70vw]' : 'top-0 left-0'}`}
                     />
                     <label htmlFor="menuToggle" className="block cursor-pointer">
                         <div className={`w-8 h-1 bg-black my-1 transition-transform ${isOpen ? 'transform rotate-45 translate-y-2' : ''}`}></div>
@@ -29,7 +30,6 @@ const HamburgerMenu = () => {
                     <ul className={`absolute top-0 left-0 w-[70vw] h-[100vh] bg-white transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
                         {['Home', 'About', 'Info', 'Contact'].map((item, index) => (
                             <a href="#" key={index} className="block p-4 border-b border-gray-200">
-                           
                                 <li>{item}</li>
                             </a>
                         ))}
