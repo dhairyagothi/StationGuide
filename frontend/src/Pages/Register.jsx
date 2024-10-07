2ee1461f15fea413e7beb04f1ed85a0945ae151e
Praveen Uppar <praveenuppar718@gmail.com>
2024-10-07 19:29:10 +0530

    Checking 10-digit phone number

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -43,18 +43,30 @@ const Register = () => {
                 </div>
 
                 {/* Phone Number Input */}
-                <div className="mb-4">
-                    <label className="block text-gray-700 font-medium mb-1" htmlFor="phoneNumber">Phone Number</label>
-                    <input
-                        type="tel"
-                        id="phoneNumber"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                        placeholder="Enter your phone number"
-                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
-                        required
-                    />
-                </div>
+                 <div className="mb-4">
+          <label
+            className="block text-gray-700 font-medium mb-1"
+            htmlFor="phoneNumber"
+          >
+            Phone Number
+          </label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="Enter your phone number"
+            pattern="\d{10}"
+            maxLength="10"
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+            required
+          />
+          {phoneNumber && phoneNumber.length !== 10 && (
+            <p className="text-red-500 text-sm">
+              Please enter a valid 10-digit phone number.
+            </p>
+          )}
+        </div>
 
                 {/* Email Input */}
                 <div className="mb-4">
