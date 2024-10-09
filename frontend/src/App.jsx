eb4d360af4ec168bcbc9d4905857c308a57c1d62
Pragyan <143019512+pragyanbhatt1213@users.noreply.github.com>
2024-10-09 16:25:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react'
 import Herosection from './Pages/Herosection'
 import LoginPage from './Pages/LoginPage';
 import Register from './Pages/Register';
-import Booking from './Pages/booking';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import chatbotsvg from './assets/svg/chatbot.svg';
 import { useNavigate, Outlet } from 'react-router-dom';
 import RailwayStations from './Pages/stations';
+import Booking from './Pages/booking';
 import './App.css'
 
 function App() {
@@ -18,9 +18,8 @@ function App() {
         <Route path="/" element={<Herosection />} />
         <Route path="/Login" element={<LoginPage />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/Booking" element={<Booking />} /> 
         <Route path='/Stations' element={<RailwayStations/>} />
-        <Route path="/Booking" element={<Booking />} />
-
 
         {/* This route is just for testing protected routes it can be removed later when there is a route other than login or signup */}
         <Route element={<ProtectedRoute />}>
