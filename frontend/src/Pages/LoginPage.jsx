f5e8e789bb34742341a021d9795be5fcabbbea2c
Kratik1093 <kratikmandloi03@gmail.com>
2024-10-10 16:02:42 +0530

    username_to_email

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -38,11 +38,11 @@ const Login = () => {
 
                 {/* Username Input */}
                 <div className="mb-5">
-                    <label className="block mb-2 font-semibold text-gray-700 " htmlFor="username">Name</label>
+                    <label className="block mb-2 font-semibold text-gray-700 " htmlFor="username">Email</label>
                     <input
-                        type="text" 
-                        id="name" 
-                        value={name} 
+                        type="email" 
+                        id="email" 
+                        value={email} 
                         onChange={(e) => setUsername(e.target.value)}
                         placeholder="Enter your name"
                         className="w-full px-4 py-2 transition duration-300 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
