5417508ce20d46909a31ee5266f4bf5e3770eae6
Gauravtb2253 <gtbomble225@gmail.com>
2024-10-14 16:02:24 +0530

    added middleware

---
+++ backend/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+      Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    };
+  };
+  
+  export default asyncHandler;
