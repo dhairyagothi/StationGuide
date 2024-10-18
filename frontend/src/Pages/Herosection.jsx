a7ef33075ea23eb3af0eabc3ccb45f318f24d8f8
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-10-18 11:02:15 +0530

    responsive landing page

--- frontend/src/Pages/Herosection.jsx
+++ frontend/src/Pages/Herosection.jsx
@@ -69,7 +69,7 @@ const Herosection = () => {
                 <button type="submit" onClick={RegisterClick} className="w-20 py-2 ml-8 font-semibold text-white transition-all duration-300 ease-in-out bg-blue-500 rounded-lg shadow-md cursor-pointer hover:bg-blue-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50">Register</button>
             </div>
 
-            <div className='grid grid-cols-3 gap-4 mt-32 md:flex md:flex-row md:justify-evenly justify-items-center '>
+            <div className='grid grid-cols-3 gap-4 mt-32 pb-20 md:flex md:flex-row md:justify-evenly justify-items-center '>
             <div 
   type="submit" 
   onClick={ContributorCLick} 
