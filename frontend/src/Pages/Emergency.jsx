f16f01a396d05a83c63e966ecf7b32a41e34f527
Prem Kolte <premkolte26@gmail.com>
2024-10-18 17:23:19 +0530

    payment page

--- frontend/src/Pages/Emergency.jsx
+++ frontend/src/Pages/Emergency.jsx
@@ -4,6 +4,11 @@ const Emergency = () => {
   return (
     <div className="min-h-screen flex justify-center items-center bg-blue-100">
       <h1 className="text-3xl font-semibold text-blue-900">Emergency Page</h1>
+      <div className='flex flex-col items-center justify-center'    >
+        <h1 className='text-2xl font-semibold text-blue-900'>Emergency Contact</h1>
+        <h1 className='text-2xl font-semibold text-blue-900'>Emergency Contact</h1>
+        <h1 className='text-2xl font-semibold text-blue-900'>Emergency Contact</h1>
+      </div>
     </div>
   );
 };
