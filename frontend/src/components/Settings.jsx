a7b4eaf43c1bb0d4c7b4cfa3fecfd28c52ff96f2
Dhairya Gothi <dhairyag31@gmail.com>
2024-10-19 12:04:37 +0530

    deployment

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import backicon from '../assets/svg/backicon.svg';
 const Settings = () => {
-  return (
-    <div>Settings</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Settings
+
+const HomeClick = () => {   
+        navigate('/'); // Navigates to the home page
+    };
+  return (
+    <>
+     <button onClick={HomeClick} className='absolute top-0 left-0'>
+                <img src={backicon} alt="" className='h-[9vh]' />
+            </button>
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-3xl font-semibold text-blue-900">Settings Page</h1>
+      
+    </div>
+    </>
+  );
+};
+
+export default Settings;
