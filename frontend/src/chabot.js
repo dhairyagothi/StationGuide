d613ce5c1bf4e34bcc1f9a7691ffe4db54de3164
SrijaVuppala295 <140592148+SrijaVuppala295@users.noreply.github.com>
2024-10-22 22:26:40 +0530

    Update chabot.js

--- frontend/src/chabot.js
+++ frontend/src/chabot.js
@@ -1,5 +1,5 @@
 // src/chatbot.js
-import chatbotConfig from './config.js'; // Adjust the path as needed
+import chatbotConfig from './config'; // Adjust the path as needed
 
 window.embeddedChatbotConfig = {
     chatbotId: chatbotConfig.chatbotId,
