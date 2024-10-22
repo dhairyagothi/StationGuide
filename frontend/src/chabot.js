fc36b0429fd6e0b636bac2d392dd12b38c53c3d8
SrijaVuppala295 <srijavuppala295@gmail.com>
2024-10-22 20:36:57 +0530

    Created ChatBot

---
+++ frontend/src/chabot.js
@@ -0,0 +1,7 @@
+// src/chatbot.js
+import chatbotConfig from './config.js'; // Adjust the path as needed
+
+window.embeddedChatbotConfig = {
+    chatbotId: chatbotConfig.chatbotId,
+    domain: chatbotConfig.domain
+};
