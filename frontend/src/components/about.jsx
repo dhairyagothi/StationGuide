ee55f510fd6bb64276e03056f8b2ae021dd55a9e
Prem Kolte <premkolte26@gmail.com>
2024-10-22 15:29:50 +0530

    add heading

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -28,6 +28,23 @@ const About = () => {
           alt="Back" 
           className="h-12" />
       </button>
+
+      <h1 
+        className="text-5xl font-bold text-blue-900 mb-12 text-center shadow-md">
+        ABOUT US
+      </h1>
+
+      {/* Introduction Section */}
+      <p 
+        className="text-xl leading-relaxed text-center max-w-2xl mb-12 px-4">
+        Welcome to 
+        <span className="font-bold text-blue-600"> 
+          StationSaarthi
+        </span>, 
+        your one-stop platform designed to elevate your Indian Railway Station experience. 
+        With cutting-edge technology and user-friendly design, we aim to provide smooth and efficient 
+        travel assistance for all passengers.
+      </p>
     </div>
   );
 };
