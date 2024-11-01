aeaecb7cfb47aa9c44f9c5100393894675ce41cb
tarunkumar2005 <tarunkumar6258278@gmail.com>
2024-11-01 07:39:32 +0530

    Added the offline page when user is offline

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+          .then(registration => {
+              console.log('ServiceWorker registration successful');
+          })
+          .catch(err => {
+              console.log('ServiceWorker registration failed: ', err);
+          });
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
